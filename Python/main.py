b6cb64ab6f035860094929024afb01c118decdb6
Alireza <alirezasaadat056@gmail.com>
2024-08-14 23:26:22 -0700

    write on the screen

--- Python/main.py
+++ Python/main.py
@@ -13,10 +13,14 @@ FPS = 60
 letter = ""
 
 # list of words
-string = ""
+first_line_string = ""
+second_line_string = ""
+
+typed_string = "|"
 
 # list of the pygame font
-lst = []
+first_line_lst = []
+
 
 speed = 0
 all_letters = 0
@@ -25,19 +29,29 @@ wrong_letters = 0
 
 def get_words():
     string = ""
-    for _ in range(10):
+    # for _ in range(10):
+    #     word = random.choice(words)
+    #     string += word + " "
+    while len(string) < 50:
         word = random.choice(words)
         string += word + " "
-    string_text = pygame.font.Font(None , 40)
+    string_text = pygame.font.Font(None , 35)
     string_text = string_text.render(string , True , "white")
-    string_text_rect = string_text.get_rect(center = (WIDTH / 2 , HEIGHT / 2))
+    string_text_rect = string_text.get_rect(topleft = (250 , HEIGHT / 2))
     
     return string[:-1] , [string_text , string_text_rect]
 
 string , string_rect = get_words()
 
 def draw():
+    screen.blit(main_surface , main_surface_rect)
     screen.blit(string_rect[0] , string_rect[1])
+    
+    typed_font = pygame.font.Font(None , 35)
+    typed_font = typed_font.render(typed_string , True, "green")
+    typed_font_rect = typed_font.get_rect(topleft = (250 , HEIGHT / 2))
+    
+    screen.blit(typed_font , typed_font_rect)
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
@@ -48,6 +62,7 @@ main_surface = pygame.Surface( (900 , 180) )
 main_surface.fill((25 , 25 , 25))
 main_surface_rect = main_surface.get_rect(center = (WIDTH / 2 , HEIGHT / 2))
 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -57,8 +72,13 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 exit()
+            elif event.key == pygame.K_BACKSPACE:
+                typed_string = typed_string[:-2] + "|"
+            else:
+                typed_string = typed_string[:-1]+ event.unicode + "|"
+    print(typed_string)
     
-    screen.blit(main_surface , main_surface_rect)
     draw()
     pygame.display.update()
     clock.tick(FPS)
+
