1522518c9665a5299f7c34d41744b72e19c58465
Alireza <alirezasaadat056@gmail.com>
2024-08-18 22:32:58 -0700

    adding the end part

--- Python/main.py
+++ Python/main.py
@@ -33,7 +33,7 @@ def get_words():
         string += word + " "
     string_text = pygame.font.Font(None, 35)
     string_text = string_text.render(string, True, "white")
-    string_text_rect = string_text.get_rect(topleft=(250, HEIGHT / 2))
+    string_text_rect = string_text.get_rect(topleft=(250, HEIGHT / 2 - 50))
 
     return string, [string_text, string_text_rect]
 
@@ -46,7 +46,7 @@ typed_font = pygame.font.Font(None, 35)
 
 def check_speed():
 
-    global start_time, speed
+    global start_time, end_time, speed
 
     if not finished:
 
@@ -54,6 +54,7 @@ def check_speed():
 
         if start_time == 0 and typed_string:
             start_time = time.time()
+        return
 
 
 def draw(typed_font):
@@ -64,12 +65,12 @@ def draw(typed_font):
     speed_font = pygame.font.Font(None, 35)
     speed_font = speed_font.render(str(speed), True, "white")
     speed_font_rect = speed_font.get_rect(
-        center=(WIDTH / 2 + 200, HEIGHT / 2 + 150))
+        center=(WIDTH / 2 + 400, HEIGHT / 2 + 100))
 
     screen.blit(speed_font, speed_font_rect)
 
     typed_font = typed_font.render(typed_string, True, "green")
-    typed_font_rect = typed_font.get_rect(topleft=(250, HEIGHT / 2))
+    typed_font_rect = typed_font.get_rect(topleft=(250, HEIGHT / 2 - 50))
 
     screen.blit(typed_font, typed_font_rect)
 
@@ -82,13 +83,19 @@ clock = pygame.time.Clock()
 
 main_surface = pygame.Surface((900, 180))
 main_surface.fill((25, 25, 25))
-main_surface_rect = main_surface.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+main_surface_rect = main_surface.get_rect(center=(WIDTH / 2, HEIGHT / 2 - 50))
 
 
 speed_surface = pygame.Surface((100, 50))
 speed_surface.fill((25, 25, 25))
 speed_surface_rect = speed_surface.get_rect(
-    center=(WIDTH / 2 + 200, HEIGHT / 2 + 150))
+    center=(WIDTH / 2 + 400, HEIGHT / 2 + 100))
+
+result_surface = pygame.Surface((780, 150))
+result_surface.fill((25, 25, 25))
+result_surface_rect = result_surface.get_rect(
+    topleft=(WIDTH / 2 - 450, HEIGHT / 2 + 75))
+
 
 while True:
     screen.fill((17, 17, 17))
@@ -126,13 +133,34 @@ while True:
                             mistakes[i][3], string[len(typed_string) + i])[0]
 
                     mis_rect = mis.get_rect(
-                        topleft=(250 + distance, HEIGHT / 2))
+                        topleft=(250 + distance, HEIGHT / 2 - 50))
                     mistakes.append([mis, mis_rect, event.unicode, mis1])
+                    letter = string[len(typed_string) + len(mistakes)]
 
     if typed_string == string[:-1]:
         finished = True
-    draw(typed_font)
-    check_speed()
 
+    if finished:
+        screen.blit(result_surface, result_surface_rect)
+
+        big_speed_text = pygame.font.Font(None, 70)
+        big_speed_text = big_speed_text.render(str(speed), True, "white")
+        big_speed_text_rect = big_speed_text.get_rect(
+            center=(WIDTH / 2 - 375, HEIGHT / 2 + 150)
+        )
+        screen.blit(big_speed_text, big_speed_text_rect)
+        if not end_time:
+            end_time = time.time()
+            end_time = round(end_time - start_time)
+
+        time_result = pygame.font.Font(None, 25)
+        time_result = time_result.render(f"Time : {end_time}", True, "white")
+        time_result_rect = time_result.get_rect(
+            center=(WIDTH / 2 - 225, HEIGHT / 2 + 150))
+
+        screen.blit(time_result, time_result_rect)
+    else:
+        draw(typed_font)
+        check_speed()
     pygame.display.update()
     clock.tick(FPS)
